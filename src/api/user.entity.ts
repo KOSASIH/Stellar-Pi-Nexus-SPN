8a96f6a1b9f5b9d8f424e33f46f66f54be1d5c4d
KOSASIH <kosasihg88@gmail.com>
2024-12-06 18:51:10 +0700

    Create user.entity.ts

---
+++ src/api/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  name: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
